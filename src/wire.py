84546e710d1232bdf983f9281dcd48cf333e0681
Epholys <epholys@gmail.com>
2025-03-20 16:27:19 +0100

    paufine wires (comments, str a repr, ...)

--- src/wire.py
+++ src/wire.py
@@ -15,9 +15,7 @@ class WireExtendedState(Enum):
             case WireExtendedState.ON:
                 return True
             case WireExtendedState.UNKNOWN:
-                raise TypeError(
-                    "Something went wrong: trying to cast the UNKNOWN state to a boolean"
-                )
+                raise TypeError("Trying to cast the UNKNOWN state to a boolean.")
 
     def __int__(self) -> int:
         match self:
@@ -26,22 +24,37 @@ class WireExtendedState(Enum):
             case WireExtendedState.ON:
                 return 1
             case WireExtendedState.UNKNOWN:
-                raise TypeError(
-                    "Something went wrong: : trying to convert the UNKONWN state to an integer."
-                )
+                raise TypeError("Trying to convert the UNKONWN state to an integer.")
+
+    def __str__(self):
+        match self:
+            case WireExtendedState.OFF:
+                return "0"
+            case WireExtendedState.ON:
+                return "1"
+            case WireExtendedState.UNKNOWN:
+                return "?"
 
 
 type WireState = WireExtendedState | bool
 
 
 class Wire:
+    """Represents a Wire connecting components in a digital circuit.
+
+    This is a "bare" wire containing only an identifier, in order to build circuits. The
+    different type of wires with states used for simulation are child classes.
+
+    This class has a static id generator: it allows to have a unique id per object. It
+    is useful to quickly check the identity of wire when printing a complete circuit,
+    as they are recursive.
+    """
+
     _id_generator = itertools.count()
 
     def __init__(self):
         """
         Initialize a new wire with a unique ID.
-
-        The ID allow to quick check while debugging
         """
         self.id: int = next(Wire._id_generator)
 
@@ -53,49 +66,28 @@ class Wire:
     def state(self, value: WireState) -> None:
         raise TypeError("Trying to set the state of a bare Wire.")
 
+    def __str__(self):
+        return "X"
+
+    def __repr__(self):
+        return f"Wire(id={self.id})"
+
     def __deepcopy__(self, memo: dict[int, Any]) -> Self:
-        """
-        Create a deep copy of the wire with a new unique ID.
-
-        Args:
-            memo (Any): The memory of already copied objects.
-
-        Returns:
-            Self: A new deepcopy object.
-        """
-        # Automatically creates a new id.
         new_wire = type(self)()
         memo[id(self)] = new_wire
         return new_wire
 
-    def __repr__(self):
-        """
-        Return detailed string representation of the wire.
-
-        Useful for debugging purpose, to easily track its ID through a circuit.
-        """
-        return f"Wire(id={self.id})"
-
 
 class WireDebug(Wire):
     """
-    Represents a wire in a digital circuit that carry binary signals.
+    Implementation of a Wire in a digital circuit used for debugging.
 
-    A wire connects the components in the circuits.
-    It maintains a state (On/Off/Unknown) and has a unique identifier for tracking
-    connections throughout the circuit.
-
-    Attributes:
-        state (WireState): The current state of the wire
-        id (int): Unique identifier for tracking connections
+    Its internal state can not only be ON or OFF, but also UNKNOWN.
+    It's useful for debugging, as the unknown state during simulation indicates an error
+    in the circuit definition, or in the simulation itself.
     """
 
     def __init__(self):
-        """
-        Initialize a new wire with a unique ID.
-
-        The ID allow to quick check while debugging
-        """
         super().__init__()
         self._state: WireExtendedState = WireExtendedState.UNKNOWN
 
@@ -111,22 +103,12 @@ class WireDebug(Wire):
             self._state = WireExtendedState.ON if value else WireExtendedState.OFF
         else:
             raise TypeError(
-                f"Trying to set the value of a {type(self).__name__} to an unsupported WireState {type(value).__name__}."
+                f"Trying to set the value of a {type(self).__name__} to an"
+                f"unsupported state: {type(value).__name__}."
             )
 
     def __str__(self):
-        """
-        Return simple string representation of wire state (0/1/X).
-
-        Useful to check the simulations results.
-        """
-        match self._state:
-            case WireExtendedState.OFF:
-                return "0"
-            case WireExtendedState.ON:
-                return "1"
-            case WireExtendedState.UNKNOWN:
-                return "?"
+        return str(self._state)
 
     def __repr__(self):
         """
@@ -134,7 +116,7 @@ class WireDebug(Wire):
 
         Useful for debugging purpose, to easily track its ID through a circuit.
         """
-        return f"WireDebug(id={self.id}, state={self._state})"
+        return f"{type(self).__name__}(id={self.id}, state={repr(self._state)}"
 
 
 class WireFast(Wire):
@@ -169,7 +151,8 @@ class WireFast(Wire):
             self._state = value
         else:
             raise ValueError(
-                f"Trying to set the value of the {type(self).__name__}'s boolean state to a more complex WireState ({type(value).__name__})."
+                f"Trying to set the value of the {type(self).__name__}'s boolean state"
+                f"to a more complex WireState ({type(value).__name__})."
             )
 
     def __str__(self):

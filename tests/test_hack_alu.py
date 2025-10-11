73821b20766c8085132dcd825e5e2e9e77984cfe
Epholys <epholys@gmail.com>
2025-10-11 11:35:16 +0200

    Add tests for the Hack ALU :
    - Add tests for almost all operations
    - Refactor to avoid too much repeated code
    - Add helper function to truncate Python's int number into signed 16 bits number

--- tests/test_hack_alu.py
+++ tests/test_hack_alu.py
@@ -1,14 +1,16 @@
 import random
 from itertools import product
+from typing import Callable
 
 import pytest
 
+from nand.simulator import Simulator
 from tests.numeric_operations import bools_to_int
 from tests.parameters_enums import Project, parameter_ids
 from tests.simulators_factory import build_simulators_cases
 
 
-def bools_to_signed_int(bits: list[bool]) -> int:
+def bools2sint(bits: list[bool]) -> int:
     """Convert a little-endian list of booleans (two's complement) into a signed int."""
     n = len(bits)
     if n == 0:
@@ -25,9 +27,15 @@ def bools_to_signed_int(bits: list[bool]) -> int:
 
     return value
 
+def to_signed16(n: int) -> int:
+    """Wrap integer n to 16-bit signed range."""
+    n &= 0xFFFF  # keep only the low 16 bits
+    if n >= 0x8000:
+        n -= 0x10000  # convert to negative range
+    return n
 
 @pytest.fixture(scope="module")
-def generate_cases(seed: int = 0, n_random_ins: int = 3,) -> list[list[bool]]:
+def cases(seed: int = 0, n_random_ins: int = 3, ) -> list[list[bool]]:
     input_list: list[list[bool]] = []
     input_list.append([True for _ in range(16)])
     input_list.append([False for _ in range(16)])
@@ -47,6 +55,27 @@ def generate_cases(seed: int = 0, n_random_ins: int = 3,) -> list[list[bool]]:
 
     return cases
 
+
+
+def simulate_operation(simulators: list[Simulator], cases: list[list[bool]], flags: list[bool], assertions: Callable[[list[bool], bool, bool, list[bool]], None]) -> None:
+    alu = simulators[25]
+
+    for case in cases:
+        case = case + flags
+        result = alu.simulate(case)
+
+        if not result:
+            assert False, "Simulation Failed"
+
+        out, zr, ng = extract_result(result)
+
+        assertions(out, zr, ng, case)
+
+
+def extract_result(result: list[bool])->tuple[list[bool], bool, bool]:
+    return result[0:16], result[16], result[17]
+
+
 @pytest.mark.parametrize(
     "simulators",
     build_simulators_cases(Project.NAND2TETRIS_HACK),
@@ -54,13 +83,8 @@ def generate_cases(seed: int = 0, n_random_ins: int = 3,) -> list[list[bool]]:
     ids=parameter_ids,
 )
 class TestHackALU:
-    @staticmethod
-    def extract_result(result: list[bool])->tuple[list[bool], bool, bool]:
-        return result[0:16], result[16], result[17]
 
-    def test_zero(self, simulators, generate_cases: list[list[bool]]):
-        alu = simulators[25]
-
+    def test_zero(self, simulators, cases):
         # out(x, y) = 0
         zx = [True]
         nx = [False]
@@ -69,24 +93,16 @@ class TestHackALU:
         f = [True]
         no = [False]
 
-        for case in generate_cases:
-            case = case + zx + nx + zy + ny + f + no
-
-            result = alu.simulate(case)
-
-            if not result:
-                assert False, "Simulation Failed"
-
-            out, zr, ng = self.extract_result(result)
-
+        def assert_zero(out: list[bool], zr: bool, ng: bool, _):
             expected_out = 0
             assert bools_to_int(out) == expected_out
             assert zr
             assert not ng
 
-    def test_one(self, simulators, generate_cases: list[list[bool]]):
-        alu = simulators[25]
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_zero)
 
+
+    def test_one(self, simulators, cases):
         # out(x, y) = 1
         zx = [True]
         nx = [True]
@@ -95,26 +111,19 @@ class TestHackALU:
         f = [True]
         no = [True]
 
-        for case in generate_cases:
-            case = case + zx + nx + zy + ny + f + no
-
-            result = alu.simulate(case)
-
-            if not result:
-                assert False, "Simulation Failed"
-
-            out, zr, ng = self.extract_result(result)
-
+        def assert_one(out: list[bool], zr: bool, ng: bool, _):
             expected_out = 1
             assert bools_to_int(out) == expected_out
             assert not zr
             assert not ng
 
-    def test_negative_one(
-        self, simulators, generate_cases: list[list[bool]]
-    ):
-        alu = simulators[25]
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_one)
 
+
+
+    def test_negative_one(
+        self, simulators, cases
+    ):
         # out(x, y) = -1
         zx = [True]
         nx = [True]
@@ -123,17 +132,267 @@ class TestHackALU:
         f = [True]
         no = [False]
 
-        for case in generate_cases:
-            case = case + zx + nx + zy + ny + f + no
-
-            result = alu.simulate(case)
-
-            if not result:
-                assert False, "Simulation Failed"
-
-            out, zr, ng = self.extract_result(result)
-
+        def assert_negative_one(out: list[bool], zr: bool, ng: bool, _):
             expected_out = -1
-            assert bools_to_signed_int(out) == expected_out
+            assert bools2sint(out) == expected_out
             assert not zr
             assert ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_negative_one)
+
+
+    def test_x(
+        self, simulators, cases
+    ):
+        # out(x, y) = x
+        zx = [False]
+        nx = [False]
+        zy = [True]
+        ny = [True]
+        f = [False]
+        no = [False]
+
+        def assert_x(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x = bools2sint(case[0:16])
+            assert bools2sint(out) == x
+            assert zr if x == 0 else not zr
+            assert ng if x < 0 else not ng
+
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_x)
+
+    def test_y(self, simulators, cases):
+        # out(x, y) = y
+        zx = [True]
+        nx = [True]
+        zy = [False]
+        ny = [False]
+        f = [False]
+        no = [False]
+
+        def assert_y(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            y = bools2sint(case[16:32])
+            assert bools2sint(out) == y
+            assert zr if y == 0 else not zr
+            assert ng if y < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_y)
+
+
+    def test_not_x(
+        self, simulators, cases
+    ):
+        # out(x, y) = !x
+        zx = [False]
+        nx = [False]
+        zy = [True]
+        ny = [True]
+        f = [False]
+        no = [True]
+
+        def assert_not_x(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            not_x = [not b for b in case[0:16]]
+            not_x_int =  bools2sint(not_x)
+            assert out == not_x
+            assert zr if not_x_int == 0 else not zr
+            assert ng if not_x_int < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_not_x)
+
+
+    def test_not_y(
+        self, simulators, cases
+    ):
+        # out(x, y) = !y
+        zx = [True]
+        nx = [True]
+        zy = [False]
+        ny = [False]
+        f = [False]
+        no = [True]
+
+        def assert_not_y(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            not_y = [not b for b in case[16:32]]
+            not_y_int =  bools2sint(not_y)
+            assert out == not_y
+            assert zr if not_y_int == 0 else not zr
+            assert ng if not_y_int < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_not_y)
+
+    def test_minus_x(
+        self, simulators, cases
+    ):
+         # out(x, y) = -x
+        zx = [False]
+        nx = [False]
+        zy = [True]
+        ny = [True]
+        f = [True]
+        no = [True]
+
+        def assert_minus_x(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            minus_x = - bools2sint(case[0:16])
+            assert bools2sint(out) == minus_x
+            assert zr if minus_x == 0 else not zr
+            assert ng if minus_x < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_minus_x)
+
+    def test_minus_y(
+        self, simulators, cases
+    ):
+         # out(x, y) = -y
+        zx = [True]
+        nx = [True]
+        zy = [False]
+        ny = [False]
+        f = [True]
+        no = [True]
+
+        def assert_minus_y(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            minus_y = - bools2sint(case[16:32])
+            assert bools2sint(out) == minus_y
+            assert zr if minus_y == 0 else not zr
+            assert ng if minus_y < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_minus_y)
+
+    def test_x_plus_one(
+        self, simulators, cases
+    ):
+         # out(x, y) = x + 1
+        zx = [False]
+        nx = [True]
+        zy = [True]
+        ny = [True]
+        f = [True]
+        no = [True]
+
+        def assert_x_plus_one(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x_plus_one = bools2sint(case[0:16]) + 1
+            assert bools2sint(out) == x_plus_one
+            assert zr if x_plus_one == 0 else not zr
+            assert ng if x_plus_one < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_x_plus_one)
+
+    def test_y_plus_one(
+        self, simulators, cases
+    ):
+         # out(x, y) = y + 1
+        zx = [True]
+        nx = [True]
+        zy = [False]
+        ny = [True]
+        f = [True]
+        no = [True]
+
+        def assert_y_plus_one(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            y_plus_one = bools2sint(case[16:32]) + 1
+            assert bools2sint(out) == y_plus_one
+            assert zr if y_plus_one == 0 else not zr
+            assert ng if y_plus_one < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_y_plus_one)
+
+    def test_x_minus_one(
+        self, simulators, cases
+    ):
+         # out(x, y) = x - 1
+        zx = [False]
+        nx = [False]
+        zy = [True]
+        ny = [True]
+        f = [True]
+        no = [False]
+
+        def assert_x_minus_one(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x_minus_one = bools2sint(case[0:16]) - 1
+            assert bools2sint(out) == x_minus_one
+            assert zr if x_minus_one == 0 else not zr
+            assert ng if x_minus_one < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_x_minus_one)
+
+    def test_y_minus_one(
+        self, simulators, cases
+    ):
+         # out(x, y) = y - 1
+        zx = [True]
+        nx = [True]
+        zy = [False]
+        ny = [False]
+        f = [True]
+        no = [False]
+
+        def assert_y_minus_one(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            y_minus_one = bools2sint(case[16:32]) - 1
+            assert bools2sint(out) == y_minus_one
+            assert zr if y_minus_one == 0 else not zr
+            assert ng if y_minus_one < 0 else not ng
+
+        simulate_operation(
+            simulators, cases, zx + nx + zy + ny + f + no, assert_y_minus_one
+        )
+
+    def test_x_plus_y(self, simulators, cases):
+        # out(x, y) = x + y
+        zx = [False]
+        nx = [False]
+        zy = [False]
+        ny = [False]
+        f = [True]
+        no = [False]
+
+        def assert_x_plus_y(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x = bools2sint(case[0:16])
+            y = bools2sint(case[16:32])
+            res = to_signed16(x + y)
+            assert bools2sint(out) == res
+            assert zr if res == 0 else not zr
+            assert ng if res < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_x_plus_y)
+
+    def test_x_minus_y(
+        self, simulators, cases
+    ):
+         # out(x, y) = x - y
+        zx = [False]
+        nx = [True]
+        zy = [False]
+        ny = [False]
+        f = [True]
+        no = [True]
+
+        def assert_x_minus_y(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x = bools2sint(case[0:16])
+            y = bools2sint(case[16:32])
+            res = to_signed16(x - y)
+            assert bools2sint(out) == res
+            assert zr if res == 0 else not zr
+            assert ng if res < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_x_minus_y)
+
+    def test_y_minus_x(
+        self, simulators, cases
+    ):
+         # out(x, y) = y - x
+        zx = [False]
+        nx = [False]
+        zy = [False]
+        ny = [True]
+        f = [True]
+        no = [True]
+
+        def assert_y_minus_x(out: list[bool], zr: bool, ng: bool, case: list[bool]):
+            x = bools2sint(case[0:16])
+            y = bools2sint(case[16:32])
+            res = to_signed16(y - x)
+            assert bools2sint(out) == res
+            assert zr if res == 0 else not zr
+            assert ng if res < 0 else not ng
+
+        simulate_operation(simulators, cases, zx + nx + zy + ny + f + no, assert_y_minus_x)

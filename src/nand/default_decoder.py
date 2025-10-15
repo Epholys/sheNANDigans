9dd7a87a857315a621719eddb25e0cd85a31c25b
Epholys <epholys@gmail.com>
2025-10-15 13:22:14 +0200

    Fix pyright warnings

--- src/nand/default_decoder.py
+++ src/nand/default_decoder.py
@@ -35,7 +35,7 @@ class DefaultDecoder(CircuitDecoder):
         """
         self.library = CircuitLibrary()
         self.data: list[int] = []
-        self.circuit = Circuit(-1)
+        self.circuit = DecodedCircuit(-1)
         self._build_core_gates(self.library)
         self.idx = 3  # Start after core gates
 

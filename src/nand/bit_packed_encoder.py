717fcb5497eac0c61d146471f34dda19a2a7d630
Epholys <epholys@gmail.com>
2025-10-26 13:17:54 +0100

    TODO EXPLAIN Optimizing the circuit before the BPE remove the brittleness of component adding order

--- src/nand/bit_packed_encoder.py
+++ src/nand/bit_packed_encoder.py
@@ -7,6 +7,7 @@ from nand.bits_utils import bitlength_with_offset, int2bitlist, int2bitlist_with
 from nand.circuit import Circuit, CircuitDict, Wire
 from nand.circuit_encoder import CircuitEncoder
 from nand.circuit_builder import CircuitLibrary
+from nand.circuit_optimizer import optimize
 
 
 class EncodedCircuitMetadata:
@@ -91,6 +92,10 @@ class BitPackedEncoder(CircuitEncoder):
         Orchestrates the encoding process.
         """
         self.library = library.get_all_circuits()
+        for circuit in self.library.values():
+            optimize(circuit)
+
+
         # See comment for 'max_*_bitlength' variables for explanation.
         self.circuits_bitlength: int = bitlength_with_offset(len(self.library))
 
